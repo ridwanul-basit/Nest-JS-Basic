5414017d27874a804bc9e481044499cbe4024cab
Ridwanul Basit <basitridwanul@gmail.com>
2026-07-15 01:00:37 +0600

    Customer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserController } from './user/user.controller';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { EmployeeModule } from './employee/employee.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [EmployeeModule, CustomerModule],
   controllers: [AppController, UserController, ProductController],
   providers: [AppService, ProductService],
 })
