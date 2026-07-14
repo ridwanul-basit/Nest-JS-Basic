814a88a723a14cbf917738535b6c7080ea2f858f
Ridwanul Basit <basitridwanul@gmail.com>
2026-07-15 01:19:26 +0600

    Customer service

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+    private customers: Customer[] = [];
+
+    getAllCustomers(): Customer[] {
+        return this.customers;
+    }
+
+    addCustomer(createCustomerDto: CreateCustomerDto): Customer {
+        const newCustomer: Customer = {
+            id: Date.now().toString(),
+             ...createCustomerDto,
+             
+        };
+        this.customers.push(newCustomer);
+        return newCustomer;
+    }
+}
+
